67dcc06797305ccd255f01a1d228c7d03b538503
Ritik Raj <ritik2903raj@gmail.com>
2021-09-30 02:16:46 +0530

    react netflix website

---
+++ src/Sdata.jsx
@@ -0,0 +1,12 @@
+const Sdata=[
+    {
+        sname: "DARK",
+        imgscr: "https://wallpapercave.com/wp/wp4056410.jpg",
+        title: "A Netflix Original Series",
+        links: "https://www.netflix.com"
+        
+    },
+
+
+];
+export default Sdata;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,33 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import  ReactDOM  from "react-dom";
+import Card from "./Cards"
+import Sdata from "./Sdata"
+import "./index.css";
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+<>
+<h1 className="heading_style">My Favourite Web Series!</h1>
+<Card
+ imgsrc={Sdata[0].imgscr}
+ title={Sdata[0].title}
+ sname={Sdata[0].sname}
+ link={Sdata[0].links}
+ />
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ <Card
+ imgsrc="https://www.hdwallpaper.nu/wp-content/uploads/2019/10/lucifer-1.jpg"
+ title="A Netflix Original Series"
+ sname="LUCIFER"
+ link="https://www.netflix.com"
+ />
+ <Card
+ imgsrc="https://akm-img-a-in.tosshub.com/indiatoday/images/story/201902/mirzapur_thumb.jpeg?FDKkc_RBidQNSMawskkTHeTlDIVsNZQt"
+ title="A Amazon Original Series"
+ sname="MIRZAPUR"
+ link="https://www.netflix.com"
+ />
+
+</>,
+document.getElementById("root")
+);
