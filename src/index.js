111e51eb3c787af466302bff80fa3d40331a82c3
Ritik Raj <ritik2903raj@gmail.com>
2021-09-30 18:50:26 +0530

    map in react

--- src/index.js
+++ src/index.js
@@ -5,36 +5,21 @@ import Sdata from "./Sdata"
 import "./index.css";
 
 
+function ncard(val){
+    return(
+        <Card
+         imgsrc={val.imgscr}
+         title={val.title}
+         sname={val.sname}
+         link={val.links}
+ />
+    );
+}
+
 ReactDOM.render(
 <>
 <h1 className="heading_style">My Favourite Web Series!</h1>
-<Card
- imgsrc={Sdata[0].imgscr}
- title={Sdata[0].title}
- sname={Sdata[0].sname}
- link={Sdata[0].links}
- />
-
- <Card
- imgsrc={Sdata[1].imgscr}
- title={Sdata[1].title}
- sname={Sdata[1].sname}
- link={Sdata[1].links}
- />
- <Card
- imgsrc={Sdata[2].imgscr}
- title={Sdata[2].title}
- sname={Sdata[2].sname}
- link={Sdata[2].links}
- />
- <Card
- imgsrc={Sdata[3].imgscr}
- title={Sdata[3].title}
- sname={Sdata[3].sname}
- link={Sdata[3].links}
- />
-
-
+{Sdata.map(ncard)}
 </>,
 document.getElementById("root")
 );
